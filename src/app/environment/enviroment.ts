9a742fc8edd0d6df6a7a0de542ea6b208d8b3272
JCampos05 <juan.camposingsoft@gmail.com>
2026-09-09 14:41:21 -0700

    Si

--- src/app/environment/enviroment.ts
+++ src/app/environment/enviroment.ts
@@ -1,4 +1,4 @@
-const production = false; // Cambia esto a true para usar la URL de producción
+const production = true; // Cambia esto a true para usar la URL de producción
 
 export const environment = {
     production: production,
